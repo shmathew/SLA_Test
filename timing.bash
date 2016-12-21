127a5375f9bfa9dc8d6b2db42e19984cf4e41ba6
shmathew <shibumathew2012@gmail.com>
2016-12-21 13:30:26 +0530

    Update timing.bash

--- timing.bash
+++ timing.bash
@@ -6,12 +6,14 @@ sleep 15
 rs_end_time=$(date +%T)
 srs_end_time=$(date +%s)
 
-echo $rs_start_time
-echo $rs_end_time
+echo $rs_start_time >> sla_output.txt
+echo $rs_end_time >> sla_output.txt
 
 DIFFS=$((( $srs_end_time - $srs_start_time)%60))
 DIFFM=$((( $srs_end_time - $srs_start_time)/60))
 
 
-echo "It took $DIFFM minutes and $DIFFS seconds"
+echo "It took $DIFFM minutes and $DIFFS seconds" >> sla_output.txt
+
+
 
